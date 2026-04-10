6c5aff02e97f469ddbef120615640b5d3a7ac439
Tejashree <tejashree@monocept.com>
2026-04-10 15:15:44 +0530

    Update mcp_program_tool.py

--- mcp_program_tool.py
+++ mcp_program_tool.py
@@ -2,20 +2,21 @@ from mcp.server.fastmcp import FastMCP
 from Program_Tool import ProgramTool
 from dotenv import load_dotenv
 import os
-mcp = FastMCP("program-service")
-load_dotenv()  # loads .env file
+import asyncio
+
+load_dotenv()
 API_KEY = os.getenv("FLEX_API_KEY")
 
+mcp = FastMCP("program-service")
 tool = ProgramTool(API_KEY)
 
 @mcp.tool()
 async def get_program(program_id: int):
-    """
-    Fetch program details using program ID
-    """
-    print(f"📞 Fetching program: {program_id}")
     return await tool.get_program(program_id)
 
-if __name__ == "__main__":
+async def main():
     print("✅ MCP running... waiting for requests")
-    mcp.run()
+    await mcp.run_async()
+
+if __name__ == "__main__":
+    asyncio.run(main())
